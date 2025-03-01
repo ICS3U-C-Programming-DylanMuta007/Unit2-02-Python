1f785475ee9ed03f355b7aa5456ad0426d501730
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-03-01 14:45:19 +0000

    Added

--- circ_circle_tau.py
+++ circ_circle_tau.py
@@ -6,11 +6,13 @@
 import math
 import constants
 
+
 def main():
     radius = float(input("What is the radius of your circle: "))
     print("")
 
-    print("The circumference of the circle is: {}" .format(radius * constants.TAU))
+    print("The circumference of the circle is: {}".format(radius * constants.TAU))
+
 
 if __name__ == "__main__":
     main()
--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Created by Dylan Mutabazi
 # Date :23-02-2025
-# This program Holds the constant TAU 
+# This program Holds the constant TAU
 
 TAU = 6.28
