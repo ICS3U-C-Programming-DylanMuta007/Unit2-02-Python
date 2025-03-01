614096f4a5b70e14d1971f0838fce7bf54ca9daa
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-03-01 15:03:26 +0000

    A

--- circ_circle_tau.py
+++ circ_circle_tau.py
@@ -8,9 +8,11 @@ import constants
 
 
 def main():
+# assigns user input to variable radius 
     radius = float(input("What is the radius of your circle: "))
     print("")
 
+# calculates circumference using variable radius and constant TAU
     print("The circumference of the circle is: {}".format(radius * constants.TAU))
 
 
